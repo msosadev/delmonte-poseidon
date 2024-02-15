4e4ddb2ebae61071dadc680ed090e309e92c2398
Miguel Sosa <msosadesign@gmail.com>
2024-02-15 20:58:22 +0100

    Add progress bar and add interactivity to sidebar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Article from "./components/Article/Article";
 import Header from "./components/Header/Header";
-import Navigation from "./components/Navigation/Navigation";
 
 function App() {
+  // Progress bar
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const totalHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const progress = (window.scrollY / totalHeight) * 100;
+      setScrollPercentage(progress);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="App">
+      <div className="progress-container">
+        <div className="progress-bar" style={{ width: `${scrollPercentage}%` }}></div>
+      </div>
       <Header />
-      <main className="reading-area">
-        <Navigation />
-        <Article />
-      </main>
+      <Article />
     </div>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,24 +1,30 @@
+// Sidebar.js
+import React from "react";
 import "./Navigation.css";
 
-export default function Navigation() {
+const Navigation = ({ sections, activeSection, onSectionClick }) => {
   return (
-    <nav className="navigation">
-      <h3 className="subtitle-1">Indice Contenuto</h3>
-      <ul className="first-level">
-        <li className="navigation-bold navigation-active">What is a social media calendar?</li>
-        <li className="navigation-bold">
-          How to Create a Social Media Calendar
-          <ul className="second-level">
-            <li className="navigation-regular">1. Conduct a social media audit.</li>
-            <li className="navigation-regular">2. Choose your social media channels and the content you'll post.</li>
-            <li className="navigation-regular">3. Decide what your calendar should include.</li>
-            <li className="navigation-regular">4. Get feedback.</li>
-          </ul>
-        </li>
-        <li className="navigation-bold">Social Media Content Calendar Tools to Plan Your Messaging</li>
-        <li className="navigation-bold">Section #4</li>
-        <li className="navigation-bold">Section #5</li>
-      </ul>
-    </nav>
+    <aside className="navigation-wrapper">
+      <div className="navigation">
+        <h3 className="subtitle-1">Indice Contenuto</h3>
+        <ul>
+          {sections.map((section) => (
+            <li
+              key={section.id}
+              onClick={() => onSectionClick(section.id)}
+              className={
+                activeSection === section.id
+                  ? `active ${section.subSection ? "sub-section navigation-regular":"section navigation-bold"}`
+                  : `${section.subSection ? "sub-section navigation-regular":"section navigation-bold"}`
+              }
+            >
+              {section.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </aside>
   );
-}
+};
+
+export default Navigation;
